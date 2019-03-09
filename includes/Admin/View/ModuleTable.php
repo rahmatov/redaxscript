0851d22674949aff46cc0331c1be02179e73a3e9
redaxmedia <info@redaxmedia.com>
2019-03-09 23:38:03 +0100

    Fix PHP linter

--- includes/Admin/View/ModuleTable.php
+++ includes/Admin/View/ModuleTable.php
@@ -6,8 +6,8 @@ use Redaxscript\Filesystem;
 use Redaxscript\Html;
 use Redaxscript\Module;
 use function array_diff;
-use function in_array;
 use function count;
+use function in_array;
 
 /**
  * children class to create the admin module table
@@ -122,7 +122,7 @@ class ModuleTable extends ViewAbstract
 					->addClass(!$value->status ? 'rs-admin-is-disabled' : null)
 					->addClass(!in_array($value->alias, $modulesFilesystemArray) ? 'rs-admin-is-corrupted' : null)
 					->html(
-						$tdElement->copy()->html($value->name . $adminControl->render('modules', $value->id, $value->alias,  $value->status)) .
+						$tdElement->copy()->html($value->name . $adminControl->render('modules', $value->id, $value->alias, $value->status)) .
 						$tdElement->copy()->text($value->description) .
 						$tdElement->copy()->text($value->version)
 				);
